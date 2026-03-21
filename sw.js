3dedbf12e5661936bb1f898e52b327f9b61a454c
Rcpdroid <amos.marbun86@gmail.com>
2026-03-21 21:41:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,24 @@
-/* =========================
-   AMOS AUTO UPDATE PWA
-   ========================= */
+const CACHE="amos-dashboard-v6";
 
-const VERSION = "amos-v1.0.0";
-const CACHE_NAME = VERSION;
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png"
-];
-
-/* INSTALL */
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install",e=>{
+self.skipWaiting();
 });
 
-/* ACTIVATE */
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key=>{
-          if(key !== CACHE_NAME){
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate",e=>{
+e.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.map(k=>{
+if(k!==CACHE)return caches.delete(k);
+})
+);
+})
+);
+self.clients.claim();
 });
 
-/* FETCH NETWORK FIRST */
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const clone=response.clone();
-        caches.open(CACHE_NAME)
-        .then(cache=>cache.put(event.request,clone));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
+self.addEventListener("fetch",event=>{
+event.respondWith(
+fetch(event.request).catch(()=>caches.match(event.request))
+);
 });
