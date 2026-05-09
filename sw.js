264077daf033b95f4c63d2f4bf6aef16470e0d24
Rcpdroid <amos.marbun86@gmail.com>
2026-05-09 11:35:38 +0700

    Simplify event listener syntax in sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-const CACHE_NAME = "amos-cache-v14";
-self.addEventListener("install", (e) => {
-    self.skipWaiting();
-});
+self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
